0535d132b09b8bfad54ec779fadea96553ce389b
thitiphong.s <thitiphong.s@pdkm.tech>
2023-06-06 00:25:41 +0700

    fix

--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -12,10 +12,10 @@ export default {
     API_ENV: process.env.API_ENV
   },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: "server",
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
