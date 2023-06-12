327292692fde6cfe2c7523e8a588094d51d73993
thitiphong.s <thitiphong.s@pdkm.tech>
2023-06-13 00:22:45 +0700

    fix

--- Frontend/env.js
+++ Frontend/env.js
@@ -5,12 +5,12 @@ envCf.config();
 let env = {
 	development: {
 		VERSION: '1.0.0(beta)',
-		BASE_URL:'http://ln.ichigozdata.win:8888',
+		BASE_URL:'https://ln.ichigozdata.win',
 		// BASE_URL:'http://localhost:8888',
 	},
 	production: {
 		VERSION: '1.0.1',
-		BASE_URL:'http://ln.ichigozdata.win:8888',
+		BASE_URL:'https://ln.ichigozdata.win',
 	},
 }
 
